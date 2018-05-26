bc80c3ff6518e7eb8e57dca269c32340a7697d40
Levi Titan <levi@Levis-MacBook-Pro.local>
2018-05-26 15:55:11 +0100

    Fix IE11 display

--- public/app.js
+++ public/app.js
@@ -12256,6 +12256,22 @@ let priv;
 let pub;
 let account;
 
+document.addEventListener('scatterLoaded', scatterExtension => {
+    // Scatter will now be available from the window scope.
+    // At this stage the connection to Scatter from the application is 
+    // already encrypted. 
+    const scatter = window.scatter;
+    
+    // It is good practice to take this off the window once you have 
+    // a reference to it.
+    window.scatter = null;
+     
+    // If you want to require a specific version of Scatter
+    scatter.requireVersion(3.0);
+    
+    //...
+});
+
 $("#welcome-done").on("click", function(){
 	$(".welcome-box").toggleClass("hide");
 })
